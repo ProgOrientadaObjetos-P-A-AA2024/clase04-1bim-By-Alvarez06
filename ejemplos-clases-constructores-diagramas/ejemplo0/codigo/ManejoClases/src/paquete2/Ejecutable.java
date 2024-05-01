d45e21aa6fadeecd2d63f84e997461fac52a5441
Byron Alvarez <bvalvarez1.1@gmail.com>
2024-05-01 16:33:47 -0500

    En clases

--- ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete2/Ejecutable.java
+++ ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete2/Ejecutable.java
@@ -15,7 +15,7 @@ public class Ejecutable {
      */
     public static void main(String[] args) {
         Computadora portatil = new Computadora("Intel-x19", 6.1+4.2);
-        System.out.printf("%s ; %.2f", portatil.obtenerTipoProcesador(), 
+        System.out.printf("%s - %.2f", portatil.obtenerTipoProcesador(), 
                 portatil.obtenerMemoria());
     }
     
---
+++ ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete3/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package paquete3;
+
+/**
+ *
+ * @author utpl
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        En esta clase principal se instancia un objeto de la clase computadora
+        en la cual se hace uso del constructor Computadora() por defecto ya que
+        no se ha especificado ningun otro constructor con parametros
+        */
+        Computadora computadoraTrabajo = new Computadora();
+    }
+    
+}
