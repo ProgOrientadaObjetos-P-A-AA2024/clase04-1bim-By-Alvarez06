8b65ac883bd1ee9a67b536f6e7ae5c4eaee8eda1
Byron Alvarez <bvalvarez1.1@gmail.com>
2024-05-01 16:42:03 -0500

    En clases

--- ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete1/Computadora.java
+++ ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete1/Computadora.java
@@ -9,6 +9,16 @@ public class Computadora {
     private String tipoProcesador;
     private double memoria;
     
+    /*
+    Creacion de constructores:
+    1. Constructor predeterminado, sin parametros pero con asignacion por defecto
+    de los valores.
+    2. Constructor con parametro double para asignar desde un principio el valor 
+    de memoria del objeto a crear
+    3. Constructor con parametro string para asignar desde un principio el valor
+    de tipo procesador del objeto a crear
+    4. Constructor con ambos parametros menciondos
+    */
     public Computadora(){
         tipoProcesador = "Intel - X";
         memoria = 16;
