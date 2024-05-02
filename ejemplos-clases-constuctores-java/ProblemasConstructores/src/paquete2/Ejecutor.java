391d74aed01e06ef0d098cf20137ac946ad0de49
Byron Alvarez <bvalvarez1.1@gmail.com>
2024-05-02 15:39:52 -0500

    en clase

--- ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete2/Ejecutor.java
+++ ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete2/Ejecutor.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete2;
+import java.util.Scanner;
 
 /**
  *
@@ -11,19 +7,40 @@ package paquete2;
  */
 public class Ejecutor {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
         /*Crear un objeto y presentar los datos
         Ingresar la información por teclado.
         */
         
-        Hospital h1 = new Hospital("Militar", "Loja", 1000, 3000);
+        /*
+        Informacion
+        "Militar", "Loja", 1000, 3000
+        "Militar DOS", "Loja", 1000, 3000
+        */
         
-        h1.establecerNombre("Hospital Militar Privado");
+        //Objetos instanciados
+        Hospital h1 = new Hospital();
         
-        Hospital h2 = new Hospital("Militar DOS", "Loja", 1000, 3000);
+        System.out.print("Ingrese el nombre del hospital: ");
+        String nom = sc.nextLine();
+        System.out.print("\nIngrese la ciudad del hospital: ");
+        String ciu = sc.nextLine();
+        System.out.print("\nIngrese el numero de doctores: ");
+        int numDoc = sc.nextInt();
+        System.out.print("\nIngrese el numero de enfermeros: ");
+        int numEnf = sc.nextInt();
         
-        System.out.printf("%s\n", h1);
-        System.out.printf("%s\n", h2);
+        h1.establecerNombre(nom);
+        h1.establecerCiudad(ciu);
+        h1.establecerNumeroDoctores(numDoc);
+        h1.establecerNumeroEnfermeros(numEnf);
+        
+        
+        System.out.printf("\nNombre del Hospital: %s\n"
+                + "Ciudad: %s\nN° de Doctores: %d\nN° de Enfermeros: %d\n", 
+                h1.obtenerNombre(), h1.obtenerCiudad(), h1.obtenerNumeroDoctores(),
+                h1.obtenerNumeroEnfermeros());
         
     }
 }
